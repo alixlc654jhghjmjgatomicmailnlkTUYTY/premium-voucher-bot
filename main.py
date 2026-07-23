a1ef0ded07b25180d5356ce0c10f972261ffb06f
alixlc654jhghjmjgatomicmailnlkTUYTY <fgghhfffcc@atomicmail.io>
2026-07-23 16:01:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,55 +2,53 @@ import asyncio
 import os
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery
+)
 from aiogram.filters import Command
-
-from dotenv import load_dotenv
-
-from buttons import main_menu
-
-
-load_dotenv()
 
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+ADMIN_ID = 8369041514
+
 
 bot = Bot(token=TOKEN)
-
 dp = Dispatcher()
 
 
-@dp.message(Command("start"))
-async def start(message: Message):
+# ======================
+# USER MENU
+# ======================
 
-    await message.answer(
-        """
-🤖 <b>Premium Voucher</b>
+def user_menu():
 
-سلام 👋
-به ربات ووچر خوش آمدید.
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
 
-یکی از گزینه‌های زیر را انتخاب کنید:
-        """,
-        reply_markup=main_menu(),
-        parse_mode="HTML"
-    )
+            [
+                InlineKeyboardButton(
+                    text="🎁 دریافت ووچر",
+                    callback_data="voucher"
+                )
+            ],
 
+            [
+                InlineKeyboardButton(
+                    text="💰 موجودی من",
+                    callback_data="balance"
+                ),
+                InlineKeyboardButton(
+                    text="🛒 خرید",
+                    callback_data="buy"
+                )
+            ],
 
-@dp.message()
-async def all_messages(message: Message):
-
-    await message.answer(
-        "از منوی زیر استفاده کنید 👇",
-        reply_markup=main_menu()
-    )
-
-
-async def main():
-
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+            [
+                InlineKeyboardButton(
+                    text="📦 سفارش‌های من",
+                    callback_data="orders"
+               
