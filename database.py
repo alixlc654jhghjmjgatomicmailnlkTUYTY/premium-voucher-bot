246b386d9eb32cac165d1f40348be50329e80f52
alixlc654jhghjmjgatomicmailnlkTUYTY <fgghhfffcc@atomicmail.io>
2026-07-23 16:11:49 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,48 +1,102 @@
 import sqlite3
+from datetime import datetime
 
 
-db = sqlite3.connect("bot.db")
-cursor = db.cursor()
+DB_NAME = "premium.db"
+
+
+def connect():
+    return sqlite3.connect(DB_NAME)
+
 
 
 def create_tables():
 
-    cursor.execute("""
+    db = connect()
+    cur = db.cursor()
+
+
+    # کاربران
+    cur.execute("""
     CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY,
+        user_id INTEGER PRIMARY KEY,
         username TEXT,
-        balance INTEGER DEFAULT 0
+        balance INTEGER DEFAULT 0,
+        created_at TEXT
     )
     """)
 
 
-    cursor.execute("""
+    # ووچرها
+    cur.execute("""
     CREATE TABLE IF NOT EXISTS vouchers(
-        code TEXT PRIMARY KEY,
-        value INTEGER,
-        used INTEGER DEFAULT 0
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        code TEXT UNIQUE,
+        amount INTEGER,
+        used INTEGER DEFAULT 0,
+        created_at TEXT
     )
     """)
+
 
     db.commit()
+    db.close()
 
 
 
 def add_user(user_id, username):
 
-    cursor.execute(
-        "INSERT OR IGNORE INTO users(id,username) VALUES(?,?)",
-        (user_id, username)
-    )
+    db = connect()
+    cur = db.cursor()
+
+    cur.execute("""
+    INSERT OR IGNORE INTO users
+    (user_id, username, created_at)
+    VALUES (?, ?, ?)
+    """,
+    (
+        user_id,
+        username,
+        str(datetime.now())
+    ))
 
     db.commit()
+    db.close()
 
 
 
-def get_users():
+def users_count():
 
-    cursor.execute(
+    db = connect()
+    cur = db.cursor()
+
+    cur.execute(
         "SELECT COUNT(*) FROM users"
     )
 
-    return cursor.fetchone()[0]
+    result = cur.fetchone()[0]
+
+    db.close()
+
+    return result
+
+
+
+def add_voucher(code, amount):
+
+    db = connect()
+    cur = db.cursor()
+
+    cur.execute("""
+    INSERT INTO vouchers
+    (code, amount, created_at)
+    VALUES (?, ?, ?)
+    """,
+    (
+        code,
+        amount,
+        str(datetime.now())
+    ))
+
+    db.commit()
+    db.close()
