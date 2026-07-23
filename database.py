f114102d7f004512928ed48eed0a2b32f42f2deb
alixlc654jhghjmjgatomicmailnlkTUYTY <fgghhfffcc@atomicmail.io>
2026-07-23 16:52:57 -0600

    Update database.py

--- database.py
+++ database.py
@@ -2,17 +2,15 @@ import sqlite3
 from datetime import datetime
 
 
-DATABASE = "premium_bot.db"
-
+DB = "database.db"
 
 
 def connect():
-
-    return sqlite3.connect(DATABASE)
-
+    return sqlite3.connect(DB)
 
 
-def create_tables():
+# ساخت جدول ها
+def init_db():
 
     db = connect()
     cursor = db.cursor()
@@ -21,57 +19,45 @@ def create_tables():
     # کاربران
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users(
-
         user_id INTEGER PRIMARY KEY,
-
         username TEXT,
-
         balance INTEGER DEFAULT 0,
-
-        status TEXT DEFAULT 'active',
-
-        created_at TEXT
-
+        join_date TEXT
     )
     """)
 
+
+    # محصولات
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price INTEGER,
+        stock INTEGER DEFAULT 0
+    )
+    """)
+
+
+    # سفارشات
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS orders(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        product TEXT,
+        price INTEGER,
+        status TEXT,
+        date TEXT
+    )
+    """)
 
 
     # ووچرها
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS vouchers(
-
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-        code TEXT UNIQUE,
-
-        amount INTEGER,
-
-        used INTEGER DEFAULT 0,
-
-        created_at TEXT
-
-    )
-    """)
-
-
-
-    # سفارش‌ها
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS orders(
-
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-
-        user_id INTEGER,
-
-        amount INTEGER,
-
-        status TEXT,
-
-        voucher TEXT,
-
-        created_at TEXT
-
+        code TEXT,
+        product TEXT,
+        used INTEGER DEFAULT 0
     )
     """)
 
@@ -81,87 +67,62 @@ def create_tables():
 
 
 
-
-
+# اضافه کردن کاربر
 def add_user(user_id, username):
 
     db = connect()
     cursor = db.cursor()
 
-
     cursor.execute("""
     INSERT OR IGNORE INTO users
-    (
-        user_id,
-        username,
-        created_at
-    )
-
-    VALUES(?,?,?)
-
+    VALUES(?,?,?,?)
     """,
-
     (
         user_id,
         username,
-        str(datetime.now())
+        0,
+        datetime.now().strftime("%Y-%m-%d")
     ))
-
 
     db.commit()
     db.close()
 
 
 
-
-
+# گرفتن موجودی کاربر
 def get_balance(user_id):
 
     db = connect()
     cursor = db.cursor()
-
 
     cursor.execute(
         "SELECT balance FROM users WHERE user_id=?",
         (user_id,)
     )
 
-
-    result = cursor.fetchone()
+    data = cursor.fetchone()
 
     db.close()
 
-
-    if result:
-        return result[0]
+    if data:
+        return data[0]
 
     return 0
 
 
 
-
-
+# افزایش موجودی
 def add_balance(user_id, amount):
 
     db = connect()
     cursor = db.cursor()
 
-
-    cursor.execute(
-        """
-        UPDATE users
-
-        SET balance = balance + ?
-
-        WHERE user_id=?
-
-        """,
-
-        (
-            amount,
-            user_id
-        )
-    )
+    cursor.execute("""
+    UPDATE users
+    SET balance = balance + ?
+    WHERE user_id=?
+    """,
+    (amount,user_id))
 
 
     db.commit()
@@ -169,78 +130,120 @@ def add_balance(user_id, amount):
 
 
 
-
-
-def users_count():
+# ثبت سفارش
+def add_order(user_id, product, price):
 
     db = connect()
     cursor = db.cursor()
 
+    cursor.execute("""
+    INSERT INTO orders
+    VALUES(NULL,?,?,?,?,?)
+    """,
+    (
+        user_id,
+        product,
+        price,
+        "pending",
+        datetime.now().strftime("%Y-%m-%d %H:%M")
+    ))
+
+    db.commit()
+    db.close()
+
+
+
+# اضافه کردن ووچر
+def add_voucher(code, product):
+
+    db = connect()
+    cursor = db.cursor()
+
+    cursor.execute("""
+    INSERT INTO vouchers
+    VALUES(NULL,?,?,0)
+    """,
+    (
+        code,
+        product
+    ))
+
+    db.commit()
+    db.close()
+
+
+
+# گرفتن ووچر سالم
+def get_voucher(product):
+
+    db = connect()
+    cursor = db.cursor()
+
+    cursor.execute("""
+    SELECT id,code 
+    FROM vouchers
+    WHERE product=? AND used=0
+    LIMIT 1
+    """,
+    (product,))
+
+
+    data = cursor.fetchone()
+
+
+    if data:
+
+        cursor.execute("""
+        UPDATE vouchers
+        SET used=1
+        WHERE id=?
+        """,
+        (data[0],))
+
+
+        db.commit()
+
+
+    db.close()
+
+
+    if data:
+        return data[1]
+
+    return None
+
+
+
+# آمار کاربران
+def users_count():
+
+    db = connect()
+    cursor=db.cursor()
 
     cursor.execute(
         "SELECT COUNT(*) FROM users"
     )
 
-
-    count = cursor.fetchone()[0]
-
+    x=cursor.fetchone()[0]
 
     db.close()
 
-    return count
+    return x
 
 
 
-
-
-def add_voucher(code, amount):
+# آمار سفارش
+def orders_count():
 
     db = connect()
-    cursor = db.cursor()
-
-
-    cursor.execute(
-        """
-        INSERT INTO vouchers
-        (
-            code,
-            amount,
-            created_at
-        )
-
-        VALUES(?,?,?)
-
-        """,
-
-        (
-            code,
-            amount,
-            str(datetime.now())
-        )
-    )
-
-
-    db.commit()
-    db.close()
-
-
-
-
-
-def vouchers_count():
-
-    db = connect()
-    cursor = db.cursor()
-
+    cursor=db.cursor()
 
     cursor.execute(
-        "SELECT COUNT(*) FROM vouchers"
+        "SELECT COUNT(*) FROM orders"
     )
 
-
-    count = cursor.fetchone()[0]
-
+    x=cursor.fetchone()[0]
 
     db.close()
 
-    return count
+    return x
