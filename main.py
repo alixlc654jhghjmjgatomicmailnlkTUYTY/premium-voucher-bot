23e43b249668b31e23c91cf932dd68a3bd957338
alixlc654jhghjmjgatomicmailnlkTUYTY <fgghhfffcc@atomicmail.io>
2026-07-23 15:49:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(TOKEN)
+if not TOKEN:
+    raise ValueError("BOT_TOKEN is missing")
+
+
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
+@dp.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer(
+        "🤖 Premium Voucher فعال شد!\n\n"
+        "سلام 👋\n"
+        "ربات با موفقیت روشن است."
+    )
+
+
 @dp.message()
-async def hello(message: Message):
-    await message.answer("🤖 Premium Voucher فعال شد")
+async def all_messages(message: Message):
+    await message.answer("✅ پیام دریافت شد")
+
 
 async def main():
+    print("Bot started...")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
